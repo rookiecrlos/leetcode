1597f49e514ce029763ed56703f6138c3c898fdd
rookiecarlos <carlos.e.m.castellanos@gmail.com>
2025-02-15 00:34:59 -0600

    TwoSum

---
+++ TwoSum.py
@@ -0,0 +1,10 @@
+from typing import List
+
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashMap = {}
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in hashMap:
+                return [hashMap[diff], i]
+            hashMap[n] = i
