c585c7df56c1965c80b68f1e4121ec9e3d3d7b25
rookiecrlos <carlossicaja@icloud.com>
2025-02-17 22:22:25 -0600

    Added comments to TwoSum problem

--- TwoSum.py
+++ TwoSum.py
@@ -1,10 +1,12 @@
 from typing import List
 
 def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashMap = {}
+        hashMap = {} # Hashmap to store the number and its index
 
         for i, n in enumerate(nums):
-            diff = target - n
-            if diff in hashMap:
+            diff = target - n # Calculate the difference between the target and the current number
+
+            if diff in hashMap: # If the difference is in the hashmap, return the index of the difference and the current index
                 return [hashMap[diff], i]
-            hashMap[n] = i
+            hashMap[n] = i # Add the current number and its index to the hashmap
+        return
