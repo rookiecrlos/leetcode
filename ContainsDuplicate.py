c1d7bab3b48dc2039f31f484452a3165af6ea0a8
rookiecrlos <carlossicaja@icloud.com>
2025-02-19 00:54:16 -0600

    Removed variable creation for duplicate in hash map solution

--- ContainsDuplicate.py
+++ ContainsDuplicate.py
@@ -1,14 +1,13 @@
 from typing import List
 
-# (Runtime 15ms) I used the same approach as the TwoSum problem. I created a hash map to store the values of each element in the 'nums' list and used a variable to check if a duplicate number was already stored.
+# (Runtime 9ms) I realized I didn't need to create a new variable. The same iteration can be stored in the hash map to compare the current value with the previously stored ones.
 def containsDuplicate(nums: List[int]) -> bool:
         hashMap = {} # Hashmap to store the number and its index
 
         for i in range(len(nums)):
-            duplicate = nums[i] # Get the current number
-            if duplicate in hashMap: # If the current number is in the hashmap, return True
+            if nums[i] in hashMap: # If the current number is in the hashmap, return True
                 return True
-            hashMap[duplicate] = i # Add the current number and its index to the hashmap
+            hashMap[nums[i]] = i # Add the current number and its index to the hashmap
         return False
 
 # (Runtime 7ms) I found a better way to solve this problem. Converting the nums list into a set automatically removes duplicates. Then, I simply compare the length of the original nums list with the new set. If the lengths are different, it means there were duplicates, so I return True. Otherwise, I return False.
