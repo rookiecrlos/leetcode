3c4b9f46ab98d4cb85447d78db5a281046871936
rookiecrlos <carlossicaja@icloud.com>
2025-02-22 00:03:06 -0600

    Problem 1941 resolved

---
+++ 1941. Check if All Characters Have Equal Number of Occurrences.py
@@ -0,0 +1,11 @@
+class Solution:
+    def areOccurrencesEqual(self, s: str) -> bool:
+        hashMap = {}
+
+        for c in s:
+            if c in hashMap:
+                hashMap[c] += 1
+            else:
+                hashMap[c] = 1
+
+        return len(set(hashMap.values())) == 1
